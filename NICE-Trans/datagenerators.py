9a15ffaad37b33f10d9cb851e99d3b5885de901a
Yau-Meng Wong <102400085+y4umeng@users.noreply.github.com>
2024-04-09 14:31:05 +0800

    holding data in memory?

--- NICE-Trans/datagenerators.py
+++ NICE-Trans/datagenerators.py
@@ -34,9 +34,8 @@ def gen_pairs(pairs, batch_size=1):
         # load fixed images
         X_data = []
         for idx in idx1:
-            # fixed = pairs[idx]
-            # X = np.load(fixed, allow_pickle=True)[0]
-            X = files[idx]
+            fixed = pairs[idx]
+            X = load_volfile(fixed)
             X = X[:,:,:144]
             X = np.reshape(X, (144, 192, 160))
             X = X[np.newaxis, np.newaxis, ...]
@@ -49,9 +48,8 @@ def gen_pairs(pairs, batch_size=1):
         # load moving images
         X_data = []
         for idx in idx2:
-            # moving = pairs[idx]
-            # X = np.load(moving, allow_pickle=True)[0]
-            X = files[idx]
+            moving = pairs[idx]
+            X = load_volfile(moving) 
             X = X[:,:,:144]
             X = np.reshape(X, (144, 192, 160))
             X = X[np.newaxis, np.newaxis, ...]
@@ -79,31 +77,13 @@ def load_by_name(path, name):
     return tuple(return_vals)
 
 
-def load_volfile(datafile, np_var):
-    """
-    load volume file
-    formats: nii, nii.gz, mgz, npz
-    if it's a npz (compressed numpy), variable names innp_var (default: 'vol_data')
-    """
-    assert datafile.endswith(('.nii', '.nii.gz', '.mgz', '.npz')), 'Unknown data file'
-
-    if datafile.endswith(('.nii', '.nii.gz', '.mgz')):
-        # import nibabel
-        if 'nibabel' not in sys.modules:
-            try :
-                import nibabel as nib  
-            except:
-                print('Failed to import nibabel. need nibabel library for these data file types.')
-
-        X = nib.load(datafile).get_data()
-        
-    else: # npz
-        if np_var == 'all':
-            X = X = np.load(datafile)
-        else:
-            X = np.load(datafile)[np_var]
-
-    return X
+def load_volfile(datafile):
+    with open(datafile, 'rb') as f:
+        f =  pickle.load(f)
+        print(f"pickle: {type(f)}", flush=True)
+        print(f"pickle[0]: {type(f[0])}", flush=True)
+        return f
+    # np.load(moving, allow_pickle=True)[0]
 
 
 def print_gpu_usage(note=""):
